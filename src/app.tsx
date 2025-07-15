a715ec5584df541be5dcf01ea10dd9e0f889ec59
Hamilton <hamilton.rodrigues.teixeira@gmail.com>
2025-07-15 15:01:56 -0300

    feat(frontedn): add shadcn and created romm component

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { CrateRomm } from "./pages/create-room";
+import { Room } from "./pages/room";
 
+const queryClient = new QueryClient();
 
 export function App() {
-
   return (
-    <div>"Hello World"</div>
-   )
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<CrateRomm />} />
+          <Route element={<Room />} path="/room/:roomId" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
-
